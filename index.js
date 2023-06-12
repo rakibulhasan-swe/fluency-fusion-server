7ecd191a0a2efe549b8caa21b9b5d94bab2f8540
unknown <rakibulhasan.seu.cse@gmail.com>
2023-06-12 21:26:38 +0600

    vercel deployment done

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     // database collection
     const allUser = client.db("fluencyFusion").collection("users");
